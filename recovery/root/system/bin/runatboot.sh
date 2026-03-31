40de30bd93fdc3607483652c3188b86cf0ab572f
AzzyC <azmath2000@gmail.com>
2026-03-31 23:22:08 +0100

    Revert "peridot: Use source's log pruning function"
    - Reverts commit 46ce99c
    - auto-dfe saves own log before the forced reboot therefore keep local function

--- recovery/root/system/bin/runatboot.sh
+++ recovery/root/system/bin/runatboot.sh
@@ -13,13 +13,25 @@ reset_touch() {
 	echo 1 > /sys/devices/platform/goodix_ts.0/reset
 }
 
+purge_logs() {
+	LOGMSG "Purging recovery-generated logs older than 14 days..."
+
+	FOX_LOG_DIR="/persist/Fox/logs"
+
+	if [ -d "$FOX_LOG_DIR" ]; then
+		find "$FOX_LOG_DIR" -maxdepth 1 -type f -mtime +14 -print -delete
+	else
+		LOGMSG "$FOX_LOG_DIR not present"
+	fi
+}
+
 SCRIPT_NAME="$(basename "$0")"
 
 LOGMSG "---$SCRIPT_NAME start---"
 
 reset_touch
 
-/sbin/prune_historic_logs.sh 10
+purge_logs
 
 LOGMSG "---$SCRIPT_NAME end---"
 exit 0
